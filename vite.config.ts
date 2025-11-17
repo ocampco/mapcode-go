00110b135c925979910f52af4e727541f110526a
ocampco <ocampco@gmail.com>
2025-11-17 15:28:49 +1100

    feat: deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// https://vite.dev/guide/static-deploy#github-pages
 export default defineConfig({
+  base: '/mapcode-go/',
   plugins: [react()],
 })
