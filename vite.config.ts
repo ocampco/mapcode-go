ea18c3fdc995d9479aa274f4c97a1e3e3af39e6d
ocampco <ocampco@gmail.com>
2025-11-17 18:02:19 +1100

    chore: add shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 // https://vite.dev/guide/static-deploy#github-pages
+// https://ui.shadcn.com/docs/installation/vite
 export default defineConfig({
   base: '/mapcode-go/',
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
