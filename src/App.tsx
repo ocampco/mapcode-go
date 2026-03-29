06cf207d3a15a0fd684cfbe0037e65a0f4175e99
ocampco <ocampco@gmail.com>
2026-03-29 17:19:28 +1100

    feat: display search type based on query

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import './App.css'
 import { LinkSearch } from './components/LinkSearch';
 import { CoordinatesSearch } from './components/CoordinatesSearch';
+import { useSearchParams } from 'react-router';
 
-const App = () => (
-  <>
-    <CoordinatesSearch />
-    <LinkSearch />
-    {/* TODO: Add result */}
-    {/* {mapcodeResult && coordinates && <Result coordinates={coordinates} mapcode={mapcodeResult} />} */}
-  </>
-);
+enum SearchType {
+  COORDINATES = 'coordinates',
+  LINK = 'link',
+};
+
+const App = () => {
+  const [searchParams] = useSearchParams();
+  const searchType =
+    searchParams.get('type') === SearchType.COORDINATES
+      ? SearchType.COORDINATES
+      : SearchType.LINK;
+
+  return (
+    <>
+      {searchType === SearchType.COORDINATES ? <CoordinatesSearch /> : <LinkSearch />}
+      {/* TODO: Add result */}
+      {/* {mapcodeResult && coordinates && <Result coordinates={coordinates} mapcode={mapcodeResult} />} */}
+    </>
+  )
+};
 
 export default App;
