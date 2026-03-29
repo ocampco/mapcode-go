bba72bc87440996ba8f88605e97c3e06e1f63dc3
ocampco <ocampco@gmail.com>
2026-03-29 16:28:54 +1100

    refactor: simplify routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,13 @@
+import App from "./App";
 import { Layout } from "./components/Layout";
-import { BASE_PATH, ROUTES } from "./constants/routes";
+import { BASE_PATH } from "./constants/routes";
 import { BrowserRouter, Route, Routes } from 'react-router';
 
 export const Router = () => (
     <BrowserRouter basename={BASE_PATH}>
         <Routes>
             <Route element={<Layout />}>
-                {ROUTES.map(({ path, element }) => (
-                    <Route key={path} path={path} element={element} />
-                ))}
+                <Route index element={<App />} />
             </Route>
         </Routes>
     </BrowserRouter>
