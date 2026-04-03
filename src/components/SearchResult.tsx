358b5236ad4f20c75c73cf12deb8d103381afae7
ocampco <ocampco@gmail.com>
2026-04-03 19:16:54 +1100

    refactor: reuse link search for consistency

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -1,63 +1,18 @@
-import { Button } from "./ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
-import { Input } from "./ui/input";
-import { Label } from "./ui/label";
-import { Spinner } from "./ui/spinner";
-import { useNavigate, useSearchParams } from "react-router";
-import { useState } from "react";
+import { useSearchParams } from "react-router";
 import { Result } from "./Result";
 import { ErrorAlert } from "./ErrorAlert";
 import { useMapcodeFromUrl } from "@/hooks/useMapcodeFromUrl";
+import { LinkSearch } from "./LinkSearch";
 
-// TODO: Handle no link
 export const SearchResult = () => {
   const [searchParams] = useSearchParams();
   const searchLink = searchParams.get('link') || '';
-  const { coordinates, mapcode, isLoading, error } = useMapcodeFromUrl(searchLink);
-  const [inputLink, setInputLink] = useState<string>(searchLink);
-  const navigate = useNavigate();
-
-  const handleSearch = () => {
-    const isPendingSearch = inputLink.trim() !== searchLink;
-    
-    if (!isPendingSearch) {
-      return;
-    }
-
-    navigate(`/?link=${encodeURIComponent(inputLink)}`);
-  }
+  const { coordinates, mapcode, error } = useMapcodeFromUrl(searchLink);
 
     return (
       <>
         <ErrorAlert error={error} />
-        <Card>
-          <CardHeader>
-            <CardTitle>Mapcode using Google Maps link 🔗</CardTitle>
-            <CardDescription>Enter the link of the location you want to find the mapcode for</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-2">
-                <Label>Google Maps Link</Label>
-                <Input
-                  type="text"
-                  required
-                  value={inputLink}
-                  onChange={(e) => setInputLink(e.target.value)}
-                  placeholder="e.g. https://maps.app.goo.gl/BwMn67pGKoqUsz5m6"
-                  autoComplete="off"
-                />
-                
-              </div>
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button size="lg" type="submit" disabled={isLoading || !inputLink} className="w-full" onClick={handleSearch}>
-              {isLoading && <Spinner data-icon="inline-start" />}
-              GO get mapcode
-            </Button>
-          </CardFooter>
-        </Card>
+        <LinkSearch />
         <Result coordinates={coordinates} mapcode={mapcode} />
       </>
     );
