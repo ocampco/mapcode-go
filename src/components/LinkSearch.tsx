428fd14a920af0066e97f98d77bd5d45e41106d8
ocampco <ocampco@gmail.com>
2026-04-03 19:13:42 +1100

    fix: search on submit instead of having to click a button

--- src/components/LinkSearch.tsx
+++ src/components/LinkSearch.tsx
@@ -11,6 +11,11 @@ export const LinkSearch = () => {
   const searchLink = searchParams.get('link') || '';
   const [inputLink, setInputLink] = useState<string>(searchLink);
   const navigate = useNavigate();
+  
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    navigate(`/search?link=${encodeURIComponent(inputLink.trim())}`);
+  };
 
     return (
       <Card>
@@ -18,33 +23,34 @@ export const LinkSearch = () => {
           <CardTitle>Mapcode using Google Maps link 🔗</CardTitle>
           <CardDescription>Enter the link of the location you want to find the mapcode for</CardDescription>
         </CardHeader>
-        <CardContent>
-          <div className="flex flex-col gap-6">
-            <div className="grid gap-2">
-              <Label>Google Maps Link</Label>
-              <Input
-                type="text"
-                required
-                value={inputLink}
-                onChange={(e) => setInputLink(e.target.value)}
-                placeholder="e.g. https://maps.app.goo.gl/BwMn67pGKoqUsz5m6"
-                autoComplete="off"
-              />
-              
-            </div>
-          </div>
-        </CardContent>
-        <CardFooter>
-          <Button
-            size="lg"
-            type="submit"
-            // TODO: Validate input
-            disabled={!inputLink.trim()}
-            className="w-full"
-            onClick={() => navigate(`/result?link=${encodeURIComponent(inputLink.trim())}`)}>
-            GO get mapcode
-          </Button>
-        </CardFooter>
+          <CardContent>
+            <form onSubmit={handleSubmit}>
+              <div className="flex flex-col gap-6">
+                <div className="grid gap-2">
+                    <Label>Google Maps Link</Label>
+                    <Input
+                      type="text"
+                      required
+                      value={inputLink}
+                      onChange={(e) => setInputLink(e.target.value)}
+                      placeholder="e.g. https://maps.app.goo.gl/BwMn67pGKoqUsz5m6"
+                      autoComplete="off"
+                    />
+                </div>
+              </div>
+            </form>
+          </CardContent>
+          <CardFooter>
+            <Button
+              size="lg"
+              type="submit"
+              // TODO: Validate input
+              disabled={!inputLink.trim()}
+              className="w-full"
+            >
+              GO get mapcode
+            </Button>
+          </CardFooter>
       </Card>
     );
 };
